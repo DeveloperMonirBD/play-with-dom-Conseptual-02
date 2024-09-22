e6629643611525b10504ec99bdf79f952311a982
Monir <mrmonir0558@gmail.com>
2024-09-22 13:07:29 +0600

    daraz user review section done

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,32 @@
-let sum = 0;
+// let sum = 0;
+// function clickMe() {
+//   const h2 = document.getElementById("count");
+//   sum += 1;
+//   //   sum= sum +1
+//   h2.innerText = sum;
+// }
+
+// const minusBtn = document.getElementById("minus-btn");
+
+// minusBtn.addEventListener("click", function () {
+//   const h2 = document.getElementById("count");
+//   sum -= 1;
+//   //   sum = sum -1
+//   h2.innerText = sum;
+// });
+
+
+// another process:- 
+const count = document.getElementById('count')
 function clickMe() {
-  const h2 = document.getElementById("count");
-  sum += 1;
-  //   sum= sum +1
-  h2.innerText = sum;
+  let currentCount = Number(count.innerText);
+  currentCount++;
+  count.innerText = currentCount;
 }
 
-const minusBtn = document.getElementById("minus-btn");
-
-minusBtn.addEventListener("click", function () {
-  const h2 = document.getElementById("count");
+const minusBtn = document.getElementById('minus-btn');
+minusBtn.addEventListener('click', function () {
+ const h2 = document.getElementById('count');
   sum -= 1;
-  //   sum = sum -1
   h2.innerText = sum;
-});
+})
--- js/review.js
+++ js/review.js
@@ -1,24 +1,11 @@
-const container = document.getElementById("review");
-document.getElementById("submit-btn").addEventListener("click", function () {
-  const inputValue = document.getElementById("text-area").value;
-  //   const inputMainValue = inputValue.value
-  const li = document.createElement("li");
-  li.style.fontSize = "40px";
-  li.innerText = inputValue;
-  container.appendChild(li);
-  document.getElementById("text-area").value = "";
+document.getElementById('submit-btn').addEventListener('click', function () {
+    const inputValue = document.getElementById('text-area').value;
+    const container = document.getElementById('review');
+    // const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.innerText = inputValue;
+    p.style.fontSize = '40px';
+    p.style.color = 'green';
+    container.appendChild(p);
+    document.getElementById('text-area').value = '';
 });
-
-// keybaord event
-
-const inputValue = document
-  .getElementById("text-area")
-  .addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-      const inputValue = document.getElementById("text-area").value;
-      const p = document.createElement("p");
-      p.innerText = inputValue;
-      container.appendChild(p);
-      document.getElementById("text-area").value = "";
-    }
-  });
