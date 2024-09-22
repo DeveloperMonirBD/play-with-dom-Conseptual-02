2531da37906df7a8e717c9c06022ceb2cb21bc0a
Monir <mrmonir0558@gmail.com>
2024-09-22 14:40:49 +0600

    Addeded Keyboard event in review section

--- js/review.js
+++ js/review.js
@@ -1,6 +1,7 @@
+//Mouse event:
+const container = document.getElementById('review');
 document.getElementById('submit-btn').addEventListener('click', function () {
     const inputValue = document.getElementById('text-area').value;
-    const container = document.getElementById('review');
     // const li = document.createElement('li');
     const p = document.createElement('p');
     p.innerText = inputValue;
@@ -10,16 +11,16 @@ document.getElementById('submit-btn').addEventListener('click', function () {
     document.getElementById('text-area').value = '';
 });
 
-// // keybaord event
 
-// const inputValue = document
-//   .getElementById("text-area")
-//   .addEventListener("keyup", function (event) {
-//     if (event.key === "Enter") {
-//       const inputValue = document.getElementById("text-area").value;
-//       const p = document.createElement("p");
-//       p.innerText = inputValue;
-//       container.appendChild(p);
-//       document.getElementById("text-area").value = "";
-//     }
-//   });
+// Keybaord event
+const inputValue = document.getElementById('text-area').addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const inputValue = document.getElementById('text-area').value;
+        const p = document.createElement('p');
+        p.innerText = inputValue;
+        p.style.fontSize = '40px';
+        p.style.color = 'green';
+        container.appendChild(p);
+        document.getElementById('text-area').value = '';
+    }
+});
